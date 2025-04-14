bdbbbfa2db8565bb5a748edb2c470e72fa257142
Alevtina <wimoon@yandex.ru>
2025-04-14 19:10:39 +0400

    -

---
+++ index.js
@@ -0,0 +1,16 @@
+const task = 'сдать экзамен f1'
+let completedTaskCount = 0;
+
+function showTask(){
+    if (!task){
+        console.log('Задача отсутствует')
+    } else {
+        console.log(task) 
+    }
+   
+}
+
+function setTask(taskDescription) {
+    
+}
+showTask()
